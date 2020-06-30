699cac8b40efb43fba58a9611f24270b5c6794f3
Sam Lawhon <67020783+slawhon213@users.noreply.github.com>
2020-06-29 20:58:18 -0600

    Better website structure (#8)
    * Better website structure
    
    * So many website changes
    
    * Refactored some css stuff
    
    * Altered homepage
    
    * Altered homepage round 2, so aesthetic
    
    * Fixed stuff
    
    Co-authored-by: Samuel Lawhon <samuellawhon@Samuels-MacBook-Air.local>

--- flaskr/server.py
+++ flaskr/server.py
@@ -5,7 +5,15 @@ import settings
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/how-it-works')
+def how_it_works():
+    return render_template('how-it-works.html')
+
 @app.route('/map')
 def map():
-    return render_template('hikingApp.html',
+    return render_template('home.html',
                            OPEN_WEATHER_API_KEY=settings.OPEN_WEATHER_API_KEY)
