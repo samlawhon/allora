5c52e9e3d58834d6b20c32891280f7314f38d3c2
Tony DiPadova <tony.19@dartmouth.edu>
2020-06-29 21:20:15 -0600

    Add pylint (#9)
    * Add pylint
    
    * Added lint job
    
    * Addressed linting
    
    * Added ESLint
    
    * Updated lint action
    
    * fix command

--- flaskr/server.py
+++ flaskr/server.py
@@ -1,19 +1,36 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+"""
+Main server routes
+"""
+from flask import Flask, render_template
 import settings
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 @app.route('/')
 def home():
+    """
+    Default home page
+    :return: rendered template
+    """
     return render_template('home.html')
+
 
 @app.route('/how-it-works')
 def how_it_works():
+    """
+    How it works page
+    :return: rendered template
+    """
     return render_template('how-it-works.html')
 
+
 @app.route('/map')
-def map():
+def render_map():
+    """
+    Rendered map
+    :return: rendered template
+    """
     return render_template('home.html',
                            OPEN_WEATHER_API_KEY=settings.OPEN_WEATHER_API_KEY)
